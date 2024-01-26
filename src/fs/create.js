8ff3c4f9151a74394c06512c626c66e532402830
Volha Melayok <neiwra@gmail.com>
2024-01-26 15:17:57 +0100

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import fs from 'node:fs';
+
 const create = async () => {
-    // Write your code here 
+  fs.stat('./files/fresh.txt', {}, (err) => {
+    if(err) {
+      const file = fs.createWriteStream('./files/fresh.txt');
+      file.write('I am fresh and young');
+    } else {
+      throw new Error(`FS operation failed`)
+    }
+  });
 };
 
 await create();
