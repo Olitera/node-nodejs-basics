80f58d1b1a1b32c1d1b1b2d02d4aa55f24a93ca7
Volha Melayok <neiwra@gmail.com>
2024-01-27 15:49:59 +0100

    feat: implement rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,15 @@
+import fs from 'node:fs';
 const rename = async () => {
-    // Write your code here 
-};
+  fs.stat('./files/wrongFilename.txt', {}, (err) => {
+    if(err) {
+      throw Error('FS operation failed')
+    }
+    fs.rename('./files/wrongFilename.txt', './files/properFilename.md', (err)=> {
+      if(err) {
+        throw Error('FS operation failed')
+      }
+    })
+  })
+}
 
 await rename();
