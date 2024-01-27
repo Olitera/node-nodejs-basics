24e51c8b5873799be8b08adc42399319746b43c4
Volha Melayok <neiwra@gmail.com>
2024-01-27 14:12:49 +0100

    feat: implement copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import * as fs from 'node:fs/promises';
+import fss from 'node:fs';
+import path from 'node:path';
+
 const copy = async () => {
-    // Write your code here 
+  fss.stat('./files', {}, (err) => {
+    if(err) {
+      throw Error('FS operation failed')
+    }
+    fs.mkdir(path.join('./files_copy'))
+      .then(() => fs.readdir(path.join('./files')))
+      .then((filesArray) => {
+        for (const file of filesArray) {
+          fs.copyFile(`${ path.join('./files', file) }`, `${ path.join('./files_copy', file) }`);
+        }
+      })
+      .catch(() => {
+        throw Error('FS operation failed')
+      })
+  })
 };
 
 await copy();
