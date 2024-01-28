e4d1df28093b456aef2392036f2eadc90527d0eb
Volha Melayok <neiwra@gmail.com>
2024-01-28 18:37:02 +0100

    feat: implement list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
 const list = async () => {
-    // Write your code here 
-};
+    fs.readdir('./files', (err, files) => {
+      if (err) {
+        throw Error('FS operation failed')
+      }
+      for (let i = 0; i < files.length; i++) {
+        fs.stat('./files' + '/'+ files[i], (err, stats) => {
+          if (stats.isFile()) {
+            console.log(`${path.basename(files[i], path.extname(files[i]))}`)
+          }
+        })
+        }
+      })
+}
 
 await list();
