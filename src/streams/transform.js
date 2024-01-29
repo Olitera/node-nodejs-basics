e93c101f1f1e2c85476d5d766a159f3c2f982a95
Volha Melayok <neiwra@gmail.com>
2024-01-29 22:15:20 +0100

    feat: implement transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import {Transform} from 'node:stream'
+
 const transform = async () => {
-    // Write your code here 
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const data = chunk.toString().split('').reverse().join('');
+      this.push(data);
+      callback();
+    }
+  });
+  process.stdin.pipe(transformStream);
+  transformStream.on('data', (data)=> process.stdout.write(data.toString() + `\r\n`))
 };
 
 await transform();
