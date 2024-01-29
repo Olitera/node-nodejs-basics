ee1ca75cab047c761f0b5d986b5b3b5143d08fd5
Volha Melayok <neiwra@gmail.com>
2024-01-29 22:30:39 +0100

    feat: implement compress.js and decompress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+import fs from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import zlib from 'node:zlib';
+import path from "node:path";
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const compress = async () => {
-    // Write your code here 
+  await pipeline(
+    fs.createReadStream(path.join(__dirname, 'files', 'fileToCompress.txt')),
+    zlib.createGzip(),
+    fs.createWriteStream(path.join(__dirname, 'files', 'archive.gz')))
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import fs from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import zlib from 'node:zlib';
+import path from "node:path";
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+  await pipeline(
+    fs.createReadStream(path.join(__dirname, 'files', 'archive.gz')),
+    zlib.createUnzip(),
+    fs.createWriteStream(path.join(__dirname, 'files', 'fileToCompress.txt')))
 };
 
 await decompress();
